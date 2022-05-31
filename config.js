28c2d9a7020a53a06ad79ff695a70b043d0cbd14
thutku <106577720+thutku@users.noreply.github.com>
2022-05-31 13:28:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'em bé à!',
+    introDesc: `anh xin lũi
+    Mặc dù hong biết làm sai gì hết
+    Nma đừng giận anh nữa nhe:<
+    Iu em lắm nên làm tặng để bù em nè:<`,
+    btnIntro: ':3',
+    title: 'Đừng giận anh nữa nhe',
+    desc: ' giận nữa là anh khok đóo ',
+    btnYes: ' oki hết giận anh rồi mà',
+    btnNo: 'không hết giận anh đâu',
+    question:' ỏ iu em lắm mãi iu em',
+    btnReply: 'dạ iu anh',
+    reply: 'yêu anh nhắmmm',
+    mess: 'Anh biết mà. Yêu em nhiều nhiều 😘😘',
+    messDesc: ' chúng ta cùng nhau xây lâu đài tình ái nhe',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
